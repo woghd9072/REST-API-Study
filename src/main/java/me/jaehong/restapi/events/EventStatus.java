c46c174982e767e3d65b7185a99f5bfc8b93decb
woghd9072 <woghd9072@gmail.com>
2019-09-29 19:40:30 +0900

    Create Domain & Test

---
+++ src/main/java/me/jaehong/restapi/events/EventStatus.java
@@ -0,0 +1,6 @@
+package me.jaehong.restapi.events;
+
+public enum EventStatus {
+
+    DRAFT, PUBLISHED, BEGAN_ENROLLMENT;
+}
---
+++ src/test/java/me/jaehong/restapi/events/EventTest.java
@@ -0,0 +1,34 @@
+package me.jaehong.restapi.events;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class EventTest {
+
+    @Test
+    public void builder() {
+        Event event = Event.builder()
+                .name("Spring REST API")
+                .description("REST API development")
+                .build();
+        assertThat(event).isNotNull();
+    }
+
+    @Test
+    public void javaBean() {
+        // Given
+        String name = "Event";
+        String description = "Spring";
+
+        // When
+        Event event = new Event();
+        event.setName("Event");
+        event.setDescription("Spring");
+
+        // Then
+        assertThat(event.getName()).isEqualTo(name);
+        assertThat(event.getDescription()).isEqualTo(description);
+    }
+
+}
