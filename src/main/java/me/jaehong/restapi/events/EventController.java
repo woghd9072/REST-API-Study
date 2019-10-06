52670fb34dcda1c851eae275c20ce7bd6bc33460
woghd9072 <woghd9072@gmail.com>
2019-10-06 17:15:57 +0900

    Business logic tests

--- src/main/java/me/jaehong/restapi/events/EventController.java
+++ src/main/java/me/jaehong/restapi/events/EventController.java
@@ -42,9 +42,9 @@ public class EventController {
         }
 
         Event event = modelMapper.map(eventDto, Event.class);
+        event.update();
         Event newEvent = this.eventRepository.save(event);
         URI createdUri = linkTo(EventController.class).slash(newEvent.getId()).toUri();
-        event.setId(10);
         return ResponseEntity.created(createdUri).body(event);
     }
 }
