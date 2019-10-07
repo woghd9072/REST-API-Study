3d2052ae8736e2ee3e93def06f0a083722075749
woghd9072 <woghd9072@gmail.com>
2019-10-08 00:01:32 +0900

    Apply Spring HATEOAS

---
+++ src/main/java/me/jaehong/restapi/events/EventResource.java
@@ -0,0 +1,15 @@
+package me.jaehong.restapi.events;
+
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.ResourceSupport;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+
+public class EventResource extends Resource<Event> {
+
+    public EventResource(Event event, Link... links) {
+        super(event, links);
+        add(linkTo(EventController.class).slash(event.getId()).withSelfRel());
+    }
+}
