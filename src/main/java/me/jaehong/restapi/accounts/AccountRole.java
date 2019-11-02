ffde59996be88e4c07a7a2622a09fdd9d92cf3f2
woghd9072 <woghd9072@gmail.com>
2019-11-02 21:16:19 +0900

    Add Acount domain

---
+++ src/main/java/me/jaehong/restapi/accounts/AccountRole.java
@@ -0,0 +1,5 @@
+package me.jaehong.restapi.accounts;
+
+public enum AccountRole {
+    ADMIN, USER
+}
--- src/main/java/me/jaehong/restapi/events/Event.java
+++ src/main/java/me/jaehong/restapi/events/Event.java
@@ -1,6 +1,7 @@
 package me.jaehong.restapi.events;
 
 import lombok.*;
+import me.jaehong.restapi.accounts.Account;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -27,6 +28,8 @@ public class Event {
     private boolean free;
     @Enumerated(EnumType.STRING)
     private EventStatus eventStatus = EventStatus.DRAFT;
+    @ManyToOne
+    private Account manager;
 
     public void update() {
         //Update free
