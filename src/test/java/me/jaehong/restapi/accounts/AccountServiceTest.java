615982d9327db2faab01ab7f5f05ea89d3ab1275
woghd9072 <woghd9072@gmail.com>
2019-11-03 21:14:11 +0900

    Test exceptions

---
+++ src/test/java/me/jaehong/restapi/accounts/AccountServiceTest.java
@@ -0,0 +1,64 @@
+package me.jaehong.restapi.accounts;
+
+import org.hamcrest.Matchers;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("test")
+public class AccountServiceTest {
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
+    @Autowired
+    AccountService accountService;
+
+    @Autowired
+    AccountRepository accountRepository;
+
+    @Test
+    public void findByUsername() {
+        //Given
+        String username = "jaehong@email.com";
+        String password = "jaehong";
+        Account account = Account.builder()
+                .email(username)
+                .password(password)
+                .roles(Set.of(AccountRole.ADMIN, AccountRole.USER))
+                .build();
+        this.accountRepository.save(account);
+
+        //When
+        UserDetailsService userDetailsService = accountService;
+        UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+
+        //Then
+        assertThat(userDetails.getPassword()).isEqualTo(password);
+    }
+
+    @Test
+    public void findByUsernameFail() {
+        //Expected
+        String username = "random@email.com";
+        expectedException.expect(UsernameNotFoundException.class);
+        expectedException.expectMessage(Matchers.containsString(username));
+
+        //When
+        accountService.loadUserByUsername(username);
+    }
+}
