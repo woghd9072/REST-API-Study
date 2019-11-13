11ea043cb137ae8a93d0b4f7199ca520fa5bb1c4
woghd9072 <woghd9072@gmail.com>
2019-11-13 22:09:16 +0900

    Config authentication server

---
+++ src/test/java/me/jaehong/restapi/configs/AuthServerConfigTest.java
@@ -0,0 +1,51 @@
+package me.jaehong.restapi.configs;
+
+import me.jaehong.restapi.accounts.Account;
+import me.jaehong.restapi.accounts.AccountRole;
+import me.jaehong.restapi.accounts.AccountService;
+import me.jaehong.restapi.common.BaseControllerTest;
+import me.jaehong.restapi.common.TestDescription;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Set;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class AuthServerConfigTest extends BaseControllerTest {
+
+    @Autowired
+    AccountService accountService;
+
+    @Test
+    @TestDescription("인증 토큰을 발급 받는 테스트")
+    public void getAuthToken() throws Exception {
+        //Given
+        String username = "jaehong@email.com";
+        String password = "jaehong";
+        Account jaehong = Account.builder()
+                .email(username)
+                .password(password)
+                .roles(Set.of(AccountRole.ADMIN, AccountRole.USER))
+                .build();
+        this.accountService.saveAccount(jaehong);
+
+        String clientId = "myApp";
+        String clientSecret = "pass";
+
+        this.mockMvc.perform(post("/oauth/token")
+                    .with(httpBasic(clientId, clientSecret))
+                    .param("username", username)
+                    .param("password", password)
+                    .param("grant_type", "password"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("access_token").exists());
+    }
+
+}
