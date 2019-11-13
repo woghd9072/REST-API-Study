59a563b6dce4661ebc551982aa7afb8fd8d3c6f0
woghd9072 <woghd9072@gmail.com>
2019-11-13 21:06:34 +0900

    Form authentication

---
+++ src/main/java/me/jaehong/restapi/configs/AppConfig.java
@@ -0,0 +1,48 @@
+package me.jaehong.restapi.configs;
+
+import me.jaehong.restapi.accounts.Account;
+import me.jaehong.restapi.accounts.AccountRole;
+import me.jaehong.restapi.accounts.AccountService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Set;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public ApplicationRunner applicationRunner() {
+        return new ApplicationRunner() {
+
+            @Autowired
+            AccountService accountService;
+
+            @Override
+            public void run(ApplicationArguments args) throws Exception {
+                Account jaehong = Account.builder()
+                        .email("jaehong@email.com")
+                        .password("jaehong")
+                        .roles(Set.of(AccountRole.ADMIN, AccountRole.USER))
+                        .build();
+                accountService.saveAccount(jaehong);
+            }
+        };
+    }
+}
